3f8576e352a9473ce6608c0e45771f357dfff3e2
LeonardLamparter <88286394+leolamparter@users.noreply.github.com>
2025-03-30 16:35:33 -0400

    BETA 1.2 FIRE (#5)
    * replaced fauna.js with firestore.js and all references to it
    
    * Migrated Dashboard, Add-Receipt, and parts of Report to FIRESTORE
    
    * Added JSDocs to firestore.js
    
    * Migrated the Category Manager (CM) and finnished migrating the Monthly Report (MR or R)
    
    * Updated styles across many pages, migrate subscription manager
    
    * Implement subscription logic in report page
    
    * FINAL FIXES

--- dashboard/index.js
+++ dashboard/index.js
@@ -1,10 +1,59 @@
 document.querySelector('.dashboard').classList.add('display-none')
 document.querySelector('.loader').classList.remove('display-none')
+let startTime = Date.now()
 setTimeout(() => {
   document.querySelector('.loader').classList.add('display-none')
   document.querySelector('.dashboard').classList.remove('display-none')
-}, 250)
+  console.log('Backup load executed.')
+}, 5000)
 document.querySelector('#beta_icon').innerHTML = version
+
+class DateConfig {
+  static today = () => [new Date().getDate(), new Date().getMonth() + 1] // 0 -> DATE ; 1 -> MONTH
+
+  static getWeekdaysPassed(targetDay) {
+      const today = new Date();
+      return Array.from({ length: today.getDate() }, (_, i) => new Date(today.getFullYear(), today.getMonth(), i + 1))
+          .filter(date => date.getDay() === targetDay).length;
+  }
+}
+
+class Sub {
+  static DOW = x => ({'sunday':0 , 'monday':1 , 'tuesday':2 , 'wednesday':3 , 'thursday':4 , 'friday':5 , 'saturday':6})[x]
+
+  static weekly(price, DOW) {
+      return DateConfig.getWeekdaysPassed(DOW %7)*price
+  }
+
+  static monthly(price, date) {
+      return (DateConfig.today()[0] >= date ? price : 0)
+  }
+
+  static yearly(price, date) {
+      return ((DateConfig.today()[1] == date[1] && DateConfig.today()[0] >= date[0]) ? price : 0)
+  }
+
+  static processTotal(sub) {
+      switch (sub.frequency) {
+          case 'weekly':
+              return Sub.weekly(sub.price, Sub.DOW(sub.freq_value));
+
+          case 'monthly':
+              return Sub.monthly(sub.price, sub.freq_value);
+
+          case 'yearly':
+              return Sub.yearly(sub.price, sub.freq_value);
+      
+          default:
+              console.error('ERROR: Sub.processTotal() error')
+              return;
+      }
+  }
+
+  static processGrandTotal(subs) {
+      return subs.reduce((p, n) => p+Sub.processTotal(n), 0)
+  }
+}
 
 // cookie script
 function getCookie(cname) {
@@ -64,50 +113,56 @@ let topCatAmtOBJ = document.querySelectorAll('#top-cat-amt');
 let hash = getCookie('hash');
 
 DB.u.get(hash).then((user) => {
-    if (user['premium'] == undefined) document.querySelectorAll('.premium-feature').forEach((el) => el.style.display = 'none');
-    if (user['version'] != 'beta1.2pr') {
-      alert("Welcome to Beta 1.2 (Pre-Release), We've added a few new features, hope you enjoy them!")
-      DB.u.update(hash, { 'version' : 'beta1.2pr' })
-    }
-    user['c_categories'] = user['c_categories'] || [];
-    console.log(user);
-    let totals = user.totals;
-    let currency = user['currency'];
-    if (currency.includes('*')) {
-      currency = currency.replace('*', '');
-    }
-    const after = user['currency'].includes('*');
-    if (after) {
-      document.querySelectorAll('.c_after').forEach((el) => {
-        el.innerHTML = ' ' + currency;
-        console.log(el, currency);
-      });
-      document.querySelectorAll('.c').forEach((el) => {
-        el.innerHTML = '';
-        console.log(el, currency);
-      });
-    } else {
-      document.querySelectorAll('.c').forEach((el) => {
-        el.innerHTML = currency;
-        console.log(el, currency);
-      });
-    }
-    const d = new Date();
-    let month = d.getFullYear().toString() + '-' + (d.getMonth()+1 < 10 ? "0" : '') + (d.getMonth()+1).toString();
-    let total = Object.values(user.totals).reduce((sum, category) => {
-      if (category[month]) sum += category[month];
-      return sum;
-    }, 0);
+  console.log(user);
+  if (user['version'] != version) {
+    alert(`Welcome to ${version}, ${versionNOTES}`)
+    DB.u.update(hash, { 'version' : version})
+  }
+  user['c_categories'] = user['c_categories'] || [];
+  console.log(user);
+  let currency = user['currency'] || '$';
+  if (currency.includes('*')) {
+    currency = currency.replace('*', '');
+  }
+  const after = user['currency'].includes('*');
+  if (after) {
+    document.querySelectorAll('.c_after').forEach((el) => {
+      el.innerHTML = ' ' + currency;
+      console.log(el, currency);
+    });
+    document.querySelectorAll('.c').forEach((el) => {
+      el.innerHTML = '';
+      console.log(el, currency);
+    });
+  } else {
+    document.querySelectorAll('.c').forEach((el) => {
+      el.innerHTML = currency;
+      console.log(el, currency);
+    });
+  }
+  const d = new Date();
+  let month = d.getFullYear().toString() + '-' + (d.getMonth()+1 < 10 ? "0" : '') + (d.getMonth()+1).toString();
+
+  DB.uCompute.all(hash, 'totals', month).then(async (totals) => {
+    let subscriptions = await DB.uDoc.allDocs(hash, 'subscriptions');
+    subscriptions.forEach(subscription => {
+      totals[subscription.name] = Sub.processTotal(subscription);
+    })
+    let total = Object.values(totals).reduce((sum, value) => sum + value, 0);
+    console.log(total);
+
     totalOBJ.innerHTML = total.toFixed(2).toString();
 
     let mo = new Date().toISOString().slice(0, 7);
 
-    let top3AMT = Object.values(totals).map(t => t[mo] || 0).sort((a, b) => b - a).slice(0, 3)
+    let top3AMT = Object.values(totals)
+      .sort((a, b) => b - a)
+      .slice(0, 3);
     let top3CAT = Object.keys(totals)
-        .map(key => ({ key, value: totals[key][mo] || 0 }))
-        .sort((a, b) => b.value - a.value)
-        .slice(0, 3)
-        .map(item => item.key);
+      .map(key => ({ key, value: totals[key][mo] || 0 }))
+      .sort((a, b) => b.value - a.value)
+      .slice(0, 3)
+      .map(item => item.key);
 
     let CATdict = {
       "a": "Groceries",
@@ -118,7 +173,6 @@ DB.u.get(hash).then((user) => {
       "f": "Livingware",
       "g": "Appliances",
       "h": "Gardenware",
-      "i": "Bills/Subcriptions",
       "j": "Pets",
       "k": "Health",
       "l": "Books",
@@ -127,14 +181,20 @@ DB.u.get(hash).then((user) => {
     user['c_categories'].forEach(c_category =>{
       CATdict[c_category] = c_category;
     })
-    top3CAT = top3CAT.map(v => CATdict[v]);
+    top3CAT = top3CAT.map(v => CATdict[v] || v);
+    console.log(top3CAT, top3AMT);
 
     topCatOBJ.forEach((topCatOBJ, i) => {
         topCatOBJ.innerHTML = top3CAT[i] || "N/A";
     })
     topCatAmtOBJ.forEach((topCatAmtOBJ, i) => {
-        topCatAmtOBJ.innerHTML = top3AMT[i].toFixed(2).toString();
+        topCatAmtOBJ.innerHTML = (top3AMT[i] || 0).toFixed(2).toString();
     })
+    let endTime = Date.now()
+    document.querySelector('.loader').classList.add('display-none')
+    document.querySelector('.dashboard').classList.remove('display-none')
+    console.log(`Page loaded in ${endTime - startTime}ms`)
+  });
 });
 
 // shortcuts
